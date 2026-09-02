934df893cdc94ac0d1890901870d7aa1994abe7a
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-03 02:49:58 +0400

    Add Expo Router navigation shell

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="learn" />
+        <Stack.Screen name="tests" />
+        <Stack.Screen name="leaderboard" />
+        <Stack.Screen name="games" />
+      </Stack>
+    </>
+  );
+}
